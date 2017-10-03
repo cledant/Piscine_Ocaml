da4b0c36a04aec9aa72ae1fd3b6f467b8860b657
cledant <cledant@student.42.fr>
2017-10-04 01:12:48 +0200

    ex08 done

---
+++ j01/ex08/ft_sum.ml
@@ -0,0 +1,10 @@
+let ft_sum f min max =
+	let rec loop cur acc =
+		if (max < min) then nan
+		else if (min = cur) then ((f cur) +. acc)
+		else loop (cur - 1) ((f cur) +. acc)
+	in loop max 0.0
+
+let () =
+	print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10);
+	print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10)
