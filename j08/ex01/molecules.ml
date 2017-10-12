b764f4ddc4f2baeac53293174e35e05dc24888a0
cledant <cledant@student.42.fr>
2017-10-13 01:52:54 +0200

    WIP j08

--- j08/ex01/molecules.ml
+++ j08/ex01/molecules.ml
@@ -1,20 +1,18 @@
-class virtual molecule (name : string) (lst : Atoms.atom list) =
-    object
-        method virtual name : string
-        method virtual formula : string
-        val virtual _lst : Atoms.atom list
+class virtual molecule (m_name : string) (a_lst : Atoms.atom list) =
+    object (self)
+        method name = m_name
+        val _lst = a_lst
+
+        method formula = "todo"
+
 
         method to_string = self#name ^ " : " ^ self#formula
         method equal (rhs : molecule) =
-            if (self#formula = rhs#formula) then true
+            if (self#formula = rhs#formula) &&  (self#name = rhs#name) then true
             else false
     end
 
 class water =
-    object (self)
-        inherit molecule "Water"
-        val _lst = [(new Atoms.hydrogen :> Atoms.atom); (new Atoms.oxygen :> Atoms.atom); (new Atoms.hydrogen :> Atoms.atom)]
-        method name = "Water"
-        method formula = "Todo"
-
+    object
+        inherit molecule "Water" [new Atoms.hydrogen; new Atoms.oxygen; new Atoms.hydrogen]
     end 
